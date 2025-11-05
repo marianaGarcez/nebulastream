c8d8ba9538a2e6751c42b38b24eb97028a484ab9
marianaGarcez <marianamgd@gmail.com>
2025-11-05 22:33:08 +0100

    add {} in lower physical
    Signed-off-by: marianaGarcez <marianamgd@gmail.com>

--- nes-query-optimizer/src/RewriteRules/LowerToPhysical/LowerToPhysicalSelection.cpp
+++ nes-query-optimizer/src/RewriteRules/LowerToPhysical/LowerToPhysicalSelection.cpp
@@ -42,7 +42,7 @@ RewriteRuleResultSubgraph LowerToPhysicalSelection::apply(LogicalOperator logica
 
     /// Creates a physical leaf for each logical leaf. Required, as this operator can have any number of sources.
     std::vector leafes(logicalOperator.getChildren().size(), wrapper);
-    return {.root = wrapper, .leafs = leafes};
+    return {.root = wrapper, .leafs = {leafes}};
 };
 
 std::unique_ptr<AbstractRewriteRule>
